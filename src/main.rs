f5ecd2196668e7534abdb76f3b90e893d1a22818
Karol Moroz <karol@moroz.dev>
2022-09-08 16:32:26 +0200

    Successfully generate items

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ use std::fs::File;
 use std::io;
 use std::io::BufReader;
 
-use serde::Deserialize;
+use handlebars::to_json;
+use serde::{Deserialize, Serialize};
+use serde_json::value::{Map, Value as Json};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct MenuItem {
     #[serde(alias = "Nazwa potrawy (PL)")]
     name: String,
     #[serde(alias = "Kategoria")]
     category: String,
     #[serde(alias = "Cena (PLN)")]
-    price: f64,
+    price: Option<f64>,
 }
 
 fn build_reader(filename: String) -> BufReader<File> {
@@ -20,15 +22,26 @@ fn build_reader(filename: String) -> BufReader<File> {
     BufReader::new(file)
 }
 
+fn build_template_registry<'a>() -> handlebars::Handlebars<'a> {
+    let mut reg = handlebars::Handlebars::new();
+    reg.register_template_file("layout", "./templates/layout.tex.hbs")
+        .unwrap();
+    reg
+}
+
 fn main() -> io::Result<()> {
     let filename = env::args().nth(1).unwrap();
     let reader = build_reader(filename);
     let mut csv = csv::Reader::from_reader(reader);
+    let reg = build_template_registry();
 
-    for result in csv.deserialize() {
-        let result: MenuItem = result?;
-        println!("{:?}", result);
-    }
+    let items: Vec<MenuItem> = csv.deserialize().filter_map(|r| r.ok()).collect();
+
+    let mut assigns = Map::new();
+    assigns.insert("items".to_string(), to_json(&items));
+
+    let result = reg.render("layout", &assigns).unwrap();
+    println!("{result}");
 
     Ok(())
 }
