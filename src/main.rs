8804b7becfb4ac1f9f0b95be9e4c9cebd48ec8fc
Karol Moroz <karol@moroz.dev>
2022-09-08 18:42:59 +0200

    Basic formatting

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ use serde_json::value::Map;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct MenuItem {
-    #[serde(alias = "Nazwa potrawy (PL)")]
-    name: String,
-    #[serde(alias = "Kategoria")]
+    name_pl: String,
+    name_en: Option<String>,
+    description_pl: Option<String>,
+    description_en: Option<String>,
     category: String,
-    #[serde(alias = "Cena (PLN)")]
     price: Option<f64>,
 }
 
@@ -22,16 +22,30 @@ fn build_reader(filename: String) -> BufReader<File> {
     BufReader::new(file)
 }
 
+fn escape_strings<'a>(data: &'a str) -> String {
+    data.replace("&", "\\&").to_string()
+}
+
+fn do_format_price(price: f64) -> String {
+    if price == price.round() {
+        format!("{} PLN", price as i64)
+    } else {
+        let str = price.to_string().replace(".", ",");
+        format!("{} PLN", str)
+    }
+}
+
 fn build_template_registry<'a>() -> handlebars::Handlebars<'a> {
     handlebars_helper!(trim: |data: String| data.trim().replace("&amp;", "\\&"));
-    handlebars_helper!(escape: |data: String| data.replace("&", "\\&"));
+    handlebars_helper!(format_price: |data: f64| do_format_price(data));
 
     let mut reg = handlebars::Handlebars::new();
     reg.register_template_file("layout", "./templates/layout.tex.hbs")
         .unwrap();
 
     reg.register_helper("trim", Box::new(trim));
-    reg.register_escape_fn(|data: &str| data.replace("&", "\\&").to_string());
+    reg.register_helper("format_price", Box::new(format_price));
+    reg.register_escape_fn(escape_strings);
     reg
 }
 
